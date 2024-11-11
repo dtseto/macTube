cfb5f33138e4e84d5d6c5f9042bfb41fd1a02a81
dtseto <67933197+dtseto@users.noreply.github.com>
2024-11-11 08:20:29 -0800

    Initial Commit

---
+++ macTube/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  macTube
+//
+//  Created by User2 on 11/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
